023692de01de016915260eb4b526d273c50b718a
Ritesh Jadhav <jadhavritesh067@gmail.com>
2024-01-09 10:02:38 +0530

    All files added

--- app/app.py
+++ app/app.py
@@ -68,8 +68,7 @@ disease_model.eval()
 # Loading crop recommendation model
 
 crop_recommendation_model_path = 'models/RandomForest.pkl'
-crop_recommendation_model = pickle.load(
-    open(crop_recommendation_model_path, 'rb'))
+crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
 
 
 # =========================================================================================
